714526a933f8bec7775702c5c83b1bd6d337476e
Tarek Zeyad <tarekzeyad31@yahoo.com>
2023-09-12 16:21:08 +0300

    feat  adding asyncing into all events

--- Plugin/CompleteRegistrationGraphQlPlugin.php
+++ Plugin/CompleteRegistrationGraphQlPlugin.php
@@ -4,21 +4,26 @@ namespace Robusta\FacebookConversions\Plugin;
 
 use Robusta\FacebookConversions\Services\ConversionsAPI;
 use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\MessageQueue\PublisherInterface;
 
 class CompleteRegistrationGraphQlPlugin
 {
     protected $logger;
     protected $conversionsAPI;
     protected $storeManager;
-
-    public function __construct(
+    protected $publisher;
+    const   TOPIC_NAME ='robusta.facebook.registration';
+  
+        public function __construct(
         \Psr\Log\LoggerInterface $logger,
         ConversionsAPI $conversionsAPI,
-        StoreManagerInterface $storeManager
+        StoreManagerInterface $storeManager,
+        PublisherInterface $publisher
     ) {
         $this->logger = $logger;
         $this->conversionsAPI = $conversionsAPI;
         $this->storeManager = $storeManager;
+        $this->publisher = $publisher;
     }
 
     public function afterResolve($subject, $result)
@@ -58,9 +63,8 @@ class CompleteRegistrationGraphQlPlugin
                     ],
                 ],
             ];
-        
-            $this->conversionsAPI->sendEventToFacebook('CompleteRegistration', $eventData);
-        } 
+            $this->publisher->publish(self::TOPIC_NAME, json_encode($eventData));
+         } 
         catch (\Exception $e) {
             $this->logger->error($e->getMessage());
         }
@@ -68,3 +72,49 @@ class CompleteRegistrationGraphQlPlugin
         return $result;
     }
 }
+// public function afterResolve($subject, $result)
+// {
+//     if (!is_array($result) || !isset($result['customer'])) {
+//         $this->logger->warning('Unexpected result format or customer data not found.');
+//         return $result;
+//     }
+
+//     $customer = $result['customer'];
+    
+//     if (!isset($customer['email'])) {
+//         $this->logger->warning('Customer email not found in result.');
+//         return $result;
+//     }
+    
+//     try {
+//         $this->logger->info('Create Customer event in progress...');
+//         $this->logger->info('Customer email: ' . $customer['email']);
+        
+//         $currencyCode = $this->storeManager->getStore()->getCurrentCurrencyCode();
+    
+//         $eventData = [
+//             'data' => [
+//                 [
+//                     'event_name' => 'CompleteRegistration',
+//                     'event_time' => time(),
+//                     'user' => [
+//                         'email' => hash('sha256', $customer['email'])
+//                     ],
+//                     'custom_data' => [
+//                         'content_name' => 'Registration',
+//                         'currency' => $currencyCode,
+//                         'status' => 'Completed',
+//                         'value' => 0,
+//                     ],
+//                 ],
+//             ],
+//         ];
+    
+//         $this->conversionsAPI->sendEventToFacebook('CompleteRegistration', $eventData);
+//     } 
+//     catch (\Exception $e) {
+//         $this->logger->error($e->getMessage());
+//     }
+
+//     return $result;
+// }
