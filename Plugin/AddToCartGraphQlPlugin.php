e26de662e74927f1838139a6421f2be9eb12f34d
Tarek Zeyad <tarekzeyad31@yahoo.com>
2023-09-12 03:34:58 +0300

    Fixing problem in add to cart testing

--- Plugin/AddToCartGraphQlPlugin.php
+++ Plugin/AddToCartGraphQlPlugin.php
@@ -29,30 +29,52 @@ class AddToCartGraphQlPlugin
         $this->cartRepository = $cartRepository; 
     }
 
-    public function afterExecute($subject, $result, $maskedCartId, $cartItems)
+    public function afterResolve($subject, $result, $field, $context, $info, $value, $args)
     {
+        $this->logger->info('working well in afterResolve');
+        
+        $maskedCartId = $args['cartId'] ?? null;
+        $cartItemsInput = $args['cartItems'] ?? [];
+        
+        if (!is_string($maskedCartId)) {
+            $this->logger->error('Expected maskedCartId to be a string but got: ' . gettype($maskedCartId));
+            return $result;  // exit early if the type is wrong
+        }
+    
         $cartId = $this->maskedQuoteIdToQuoteId->execute($maskedCartId);
         $cart = $this->cartRepository->get($cartId);
         
         $eventsData = [];
-        foreach ($cartItems as $cartItemData) {
+        foreach ($cartItemsInput as $cartItemData) {
             try {
                 $sku = $cartItemData['sku'] ?? null;
                 $qty = $cartItemData['quantity'] ?? 0;
-
+    
                 if (!$sku) {
                     $this->logger->warning('SKU not found in cart items data.');
                     continue;
                 }
                 
-                $cartItem = $cart->getItemByProductSku($sku);
-                if (!$cartItem) {
+                // Retrieve cart item by its SKU
+                $cartItems = $cart->getAllItems();
+                $matchedItem = null;
+                foreach ($cartItems as $item) {
+                    if ($item->getProduct()->getSku() == $sku) {
+                        $matchedItem = $item;
+                        break;
+                    }
+                }
+                
+                if (!$matchedItem) {
                     $this->logger->warning('Cart item not found for SKU: ' . $sku);
                     continue;
                 }
+                $this->logger->info('cart item found for SKU: ' . $sku);
+                $cartItem = $matchedItem;
+    
                 $customerEmail = $cart->getCustomer()->getEmail();
                 $currencyCode = $this->storeManager->getStore()->getCurrentCurrencyCode();
-
+    
                 $eventData = [
                     'event_name' => 'AddToCart',
                     'event_time' => time(),
@@ -69,17 +91,18 @@ class AddToCartGraphQlPlugin
                 ];
                 
                 $eventsData[] = $eventData;
-
+    
             } catch (\Exception $e) {
                 $this->logger->error($e->getMessage());
             }
         }
-
+    
         if (!empty($eventsData)) {
             $this->conversionsAPI->sendEventToFacebook('AddToCart', ['data' => $eventsData]);
         }
-
+    
         return $result;
     }
+    
 
 }
